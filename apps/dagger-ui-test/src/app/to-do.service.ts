828795411bb319c9d8f4f4c77c11217046595595
Sascha Engmann <engmann@w11k.de>
2023-08-14 11:28:34 +0200

    refactor loading service

--- apps/dagger-ui-test/src/app/to-do.service.ts
+++ apps/dagger-ui-test/src/app/to-do.service.ts
@@ -1,6 +1,6 @@
-import {computed, Injectable, signal} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {ToDo} from "@dagger-ui-test/shared";
+import { HttpClient } from "@angular/common/http";
+import { computed, Injectable, signal } from '@angular/core';
+import { ToDo } from "@dagger-ui-test/shared";
 
 @Injectable({
   providedIn: 'root'
@@ -8,18 +8,23 @@ import {ToDo} from "@dagger-ui-test/shared";
 export class ToDoService {
 
   #toDos = signal<ToDo[]>([])
-  hasLoaded = false;
+
+  hasLoaded = false
 
   constructor(private httpClient: HttpClient) {
   }
 
   getToDos() {
     if (this.hasLoaded === false) {
-      this.httpClient.get<ToDo[]>('dagger-ui-backend/todo').subscribe(result => {
-        this.#toDos.set(result)
-        this.hasLoaded = true
-      })
+      this.update()
     }
     return computed(() => this.#toDos());
+  }
+
+  update() {
+    this.httpClient.get<ToDo[]>('dagger-ui-backend/todo').subscribe(result => {
+      this.#toDos.set(result)
+      this.hasLoaded = true
+    })
   }
 }
